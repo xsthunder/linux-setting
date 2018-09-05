8cef55b7483437d2dbe7d569e5b930408f918d0a
xsthunder <xsthunder@outlook.com>
2018-09-05 22:01:40 +0800

    set-git-proxy

---
+++ set-git-proxy.sh
@@ -0,0 +1,6 @@
+echo "Host github.com
+Hostname github.com
+User git
+ProxyCommand socat - PROXY:127.0.0.1:\%h:\%p,proxyport=1080"\
+>> ~/.ssh/config
+which socat || { echo "socat not found!" ; exit 1; }
