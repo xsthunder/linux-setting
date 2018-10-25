32969f858ce30717c9c74d2d14b57660447ee14c
xsthunder <xsthunder@outlook.com>
2018-10-25 08:57:47 +0800

    (add) move set-up-sshd.sh

--- bash-script/set-up-sshd.sh
+++ bash-script/set-up-sshd.sh
@@ -1,10 +1,12 @@
+# copy this to ~/.ssh
+# put *.pub in ~/.ssh
+# run with root user
 set -e
 apt install openssh-server -y
-service start sshd
 for i in ./*.pub
 do
 	cat $i >> authorized_keys
 done
 chmod 600 authorized_keys
 chmod 700 ./
-service restart sshd
+service start sshd
