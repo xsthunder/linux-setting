6ea5b78dfbd870372b4aa7268e49e69bc3cb1326
xsthunder <xsthunder@outlook.com>
2018-11-09 10:53:49 +0800

    (rm) extra git script

--- bash-script/git-ssh-proxy.sh
+++
@@ -1,8 +0,0 @@
-# use socks proxy hosted at localhost:1080
-# only available for ssh protocol
-echo "Host github.com
-Hostname github.com
-User git
-ProxyCommand socat - PROXY:127.0.0.1:\%h:\%p,proxyport=1080"\
->> ~/.ssh/config
-which socat > /dev/null || { echo "socat not found!" ; exit 1; }
