a986df653da0e51b805c6a7a2e2f70c3041d997d
xsthunder <xsthunder@outlook.com>
2018-11-09 10:58:02 +0800

    (add) conda-set-channel.sh

---
+++ bash-script/conda-set-channel.sh
@@ -0,0 +1,5 @@
+conda config --add channels https://mirrors6.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors6.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --set show_channel_urls yes
