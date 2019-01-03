dd77ae0aca1c8d79bd36cef757210ae90ca6c3b2
xsthunder <xsthunder@outlook.com>
2019-01-03 13:48:50 +0800

    fix conda/add-jupyter-kernel.sh start-jupyter-notebook.sh, jupyter should be lauch at base env

--- bash-script/conda/add-jupyter-kernel.sh
+++ bash-script/conda/add-jupyter-kernel.sh
@@ -9,7 +9,6 @@ conda env list | grep "$myenv" > /dev/null || {
 	echo "no such env as $myenv!";
 	exit 1;
 }
-source activate $myenv
 python -m ipykernel --version > /dev/null|| pip install ipykernel
 python -m ipykernel install --user --name $myenv --display-name "Python ($myenv)"
 
--- bash-script/start-jupyter-notebook.sh
+++ bash-script/start-jupyter-notebook.sh
@@ -1,3 +1,3 @@
 export PATH="/home/xs/anaconda3/bin:$PATH"
 cd /mnt/c/wsl-workspace/jupyter-ws
-source activate my-jupyter && jupyter notebook
+jupyter notebook
