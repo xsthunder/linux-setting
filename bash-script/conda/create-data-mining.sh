2f38d72a06c51ef7df12c28664fa401e00ff2640
xsthunder <xsthunder@outlook.com>
2019-01-21 13:55:27 +0800

    Update create-data-mining.sh

--- bash-script/conda/create-data-mining.sh
+++ bash-script/conda/create-data-mining.sh
@@ -4,4 +4,4 @@ name=data-mining
 conda create -n $name python=3.6
 source activate $name
 conda install pandas xlrd tqdm numpy ipykernel -y
-conda install scikit-learn tensorflow=1.7.0 keras=2.2.4 -y
+conda install scikit-learn tensorflow keras -y
