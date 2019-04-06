130e095d64a52e962bb69a6ec1e1b34904b2ee8a
xsthunder <xsthunder@outlook.com>
2019-04-06 13:15:55 +0800

    Update create-clean-data.sh

--- bash-script/conda/create-clean-data.sh
+++ bash-script/conda/create-clean-data.sh
@@ -4,5 +4,8 @@ name=clean-data
 conda create -n $name
 source activate $name
 conda install pandas xlrd tqdm numpy ipykernel -y
-conda install python-dateutil XlsxWriter pyodbc requests -y
+conda install python-dateutil XlsxWriter pyodbc -y
+
+# following for network support
+conda install requests -y
 conda install -c anaconda beautifulsoup4 -y
