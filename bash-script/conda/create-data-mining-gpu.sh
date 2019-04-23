63acce21e19d7f419671f9dcf392c26177307266
xsthunder <xsthunder@outlook.com>
2019-04-23 16:25:37 +0800

    Update create-data-mining-gpu.sh

--- bash-script/conda/create-data-mining-gpu.sh
+++ bash-script/conda/create-data-mining-gpu.sh
@@ -4,6 +4,6 @@ name=data-mining-gpu
 conda create -n $name python=3.6
 source activate $name
 conda install pandas xlrd tqdm numpy ipykernel -y
-conda install scikit-learn tensorflow-gpu keras-gpu -y
-
-conda install pillow -y # image support such as keras.preprocessing.image
+conda install scikit-learn tensorflow-gpu keras-gpu \
+  pillow \ # image support such as keras.preprocessing.image
+  -y
