8c9e9cdd12febadf5799aad2188f6b2a15eb3604
xsthunder <xsthunder@outlook.com>
2019-05-20 15:00:53 +0800

    add ping

--- bash-script/test_and_try_connect.sh
+++ bash-script/test_and_try_connect.sh
@@ -1,4 +1,5 @@
-url=baidu.com
+url=www.baidu.com
+ping -w 1 $url || bash ~/connect.sh
 # don't allow redirect, you'll get 302
 http_code=`curl -LI "$url" -o /dev/null -w '%{http_code}\n' -s --connect-time 3 --max-redirs 0`
-test $http_code -eq 302 && bash ./connect.sh
+test $http_code -eq 200 || bash ~/connect.sh
