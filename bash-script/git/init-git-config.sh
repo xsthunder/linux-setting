18807df10582414c55832707b6d727b7916292c0
xsthunder <xsthunder@outlook.com>
2019-06-12 15:41:46 +0800

    Update and rename global-init-var.sh to init-git-config.sh

--- bash-script/git/init-git-config.sh
+++ bash-script/git/init-git-config.sh
@@ -8,3 +8,11 @@ git config --global --add user.name xsthunder
 
 # set variance in editor
 # git config --global --edit
+
+git config –-global core.editor vim
+
+# https://www.cnblogs.com/flying_bat/p/3324769.html
+git config --global core.autocrlf false # 提交检出均不转换
+
+#提交包含混合换行符的文件时给出警告
+git config --global core.safecrlf warn
