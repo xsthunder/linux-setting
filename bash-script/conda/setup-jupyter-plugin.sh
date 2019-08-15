27c84f0b956b63c374b13f69ce437d488d469dfd
xsthunder <xsthunder@outlook.com>
2019-08-15 16:02:32 +0800

    update jupyter config script

--- bash-script/conda/setup-jupyter-plugin.sh
+++ bash-script/conda/setup-jupyter-plugin.sh
@@ -1,3 +1,6 @@
+BASH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+echo $BASH_DIR
+
 set -e
 
 # https://github.com/ipython-contrib/jupyter_contrib_nbextensions
@@ -8,9 +11,25 @@ jupyter contrib nbextension install --user
 conda install -y -c conda-forge jupyter_nbextensions_configurator
 jupyter nbextensions_configurator enable --user
 
+# install vim
 # https://github.com/lambdalisue/jupyter-vim-binding/wiki/Installation#add-vim_binding-to-ipython-notebook-extensions
 mkdir -p $(jupyter --data-dir)/nbextensions
 # Now clone the repository
 cd $(jupyter --data-dir)/nbextensions
 git clone --depth=1 https://github.com/lambdalisue/jupyter-vim-binding vim_binding
 chmod -R go-w vim_binding
+
+# set up Snippets config
+cp $BASH_DIR/config-files/snippets.json $(jupyter --data-dir)/nbextensions/snippets/snippets.json
+
+# enable plugin
+## vim_binding
+jupyter nbextension enable vim_binding/vim_binding
+## snippets
+jupyter nbextension enable snippets/main
+## freeze
+jupyter nbextension enable freeze/main
+## ExecuteTime
+jupyter nbextension enable execute_time/ExecuteTime
+## Toggle all line numbers
+jupyter nbextension enable toggle_all_line_numbers/main
