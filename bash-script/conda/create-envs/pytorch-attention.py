34c9634083b3c8962b3c389ed948c1d90d50428e
xsthunder <xsthunder@outlook.com>
2019-11-27 10:06:51 +0800

    add pytorch-attention.py

---
+++ bash-script/conda/create-envs/pytorch-attention.py
@@ -0,0 +1,7 @@
+# for https://github.com/jadore801120/attention-is-all-you-need-pytorch
+set -e
+name=create-pytorch-attention.py
+conda create -n $name python=3.6 -y
+source activate $name
+conda install pytorch torchvision cudatoolkit=10.1 -c pytorch # see https://pytorch.org/get-started/locally/
+conda install pandas xlrd tqdm numpy ipykernel -y
