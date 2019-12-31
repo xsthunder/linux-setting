b6a1796d098d00f8a20202b44fdd34f8c41e1154
xsthunder <xsthunder@outlook.com>
2019-12-31 15:15:46 +0800

    ./pytorch.sh

--- bash-script/conda/create-envs/pytorch.sh
+++ bash-script/conda/create-envs/pytorch.sh
@@ -3,5 +3,5 @@ set -e
 name=create-pytorch-attention.py
 conda create -n $name python=3.6 -y
 source activate $name
-conda install pytorch torchvision cudatoolkit=10.1 -c pytorch # see https://pytorch.org/get-started/locally/
+conda install pytorch torchvision cudatoolkit=10.1 -c pytorch -y # see https://pytorch.org/get-started/locally/
 conda install pandas xlrd tqdm numpy ipykernel -y
