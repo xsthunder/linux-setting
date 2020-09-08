64faabd5f922aa9d476e72d39d93b98698454768
xsthunder <xsthunder@outlook.com>
2020-09-08 11:26:01 +0800

    Create set-execution-proxy.ps1

---
+++ windows-bat/set-execution-proxy.ps1
@@ -0,0 +1,5 @@
+# using in administrator mode
+# see about_Execution_Policies - PowerShell | Microsoft Docs 
+# https://docs.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7#use-group-policy-to-manage-execution-policy
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
