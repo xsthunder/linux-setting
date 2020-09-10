822c9179426ec8c99a5d58b477247431b428d1f0
xsthunder <xsthunder@outlook.com>
2020-09-10 16:58:59 +0800

    Update get-bash-file-dir.sh

--- bash-script/template/get-bash-file-dir.sh
+++ bash-script/template/get-bash-file-dir.sh
@@ -1,4 +1,7 @@
 # see https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 # It will work as long as the last component of the path used to find the script is not a symlink (directory links are OK).
+
+# only getting the top level bash script dir, but not the sub bash script dir.
+
 BASH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 echo $BASH_DIR
