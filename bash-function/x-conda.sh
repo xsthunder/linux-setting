ed767c8175caab622f53e79d7a1c0c30e8c97d6a
xsthunder <xsthunder@outlook.com>
2020-12-30 10:51:15 +0800

    use return instead of exit in bash function

--- bash-function/x-conda.sh
+++ bash-function/x-conda.sh
@@ -2,7 +2,8 @@ function x-conda-grep(){
 	env_name=$1
 	if test -z $env_name
 	then
-		echo "usage $0 <fuzzy conda env name>"
+		echo "usage x-conda-grep( <fuzzy conda env name>"
+        return 1
 	fi
 	echo $(conda env list | grep $env_name | awk '{print $1}')
 }
@@ -10,9 +11,19 @@ function x-conda-act(){
 	env_name=$1
 	if test -z $env_name
 	then
-		echo "usage $0 <fuzzy conda env name>"
+		echo "usage x-conda-act( <fuzzy conda env name>"
+        return 1
 	fi
-	source activate $(x-conda-grep $env_name)
+    fuz_name=$(x-conda-grep $env_name)
+
+	if test -z $fuz_name
+    then
+        echo "$env_name not found, env list:"
+        conda env list
+        return 1
+    fi
+
+	source activate $fuz_name
 }
 export -f x-conda-grep
 export -f x-conda-act
