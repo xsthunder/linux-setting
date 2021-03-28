1af3c26da9c56cf3cd76e12d6cc20762074e5af0
xsthunder <xsthunder@outlook.com>
2021-03-28 11:44:56 +0800

    add cd .. jnb2py

--- bash-function/index.sh
+++ bash-function/index.sh
@@ -1,3 +1,5 @@
 BASH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+alias jnb2py="jupyter-nbconvert --to=script"
+alias ..="cd .."
 source $BASH_DIR/x-conda.sh
 source $BASH_DIR/x-screen.sh
