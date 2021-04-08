0f0aceb2b7a89a40a713794eaf05933b1871a18e
xsthunder <xsthunder@outlook.com>
2021-04-08 15:04:44 +0800

    add tqdm

--- bash-script/conda/create-envs/bert4keras1.15.sh
+++ bash-script/conda/create-envs/bert4keras1.15.sh
@@ -7,4 +7,4 @@ conda install pandas xlrd tqdm numpy ipykernel -y
 conda install tensorflow-gpu=1.15.0 keras=2.3.1 scikit-learn pillow matplotlib nltk -y
 
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-yes | pip install bert4keras==0.9.9 rouge
+yes | pip install bert4keras==0.9.9 rouge tqdm
