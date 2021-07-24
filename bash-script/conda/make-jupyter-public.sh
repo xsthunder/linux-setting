7f94bc8e0dee95514e0e79a9d387940ec7d1f9a2
xsthunder <xsthunder@outlook.com>
2021-07-24 12:20:47 +0800

    Update make-jupyter-public.sh

--- bash-script/conda/make-jupyter-public.sh
+++ bash-script/conda/make-jupyter-public.sh
@@ -1,1 +1,8 @@
 jupyter notebook password # set password @see https://jupyter-notebook.readthedocs.io/en/stable/public_server.html
+vim ~.jupyter/jupyter_notebook_config.json
+{
+  "NotebookApp": {
+    "password": "...",
+    "ip": "*"
+  }
+}
